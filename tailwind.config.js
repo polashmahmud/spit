58e8e8ddff0b4ff3019e692e49e07c861358e146
Polash Mahmud <polashmahmud@gmail.com>
2024-07-16 07:21:55 +0600

    fix layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,7 @@ export default {
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ['Hind Siliguri', ...defaultTheme.fontFamily.sans],
             },
         },
     },
